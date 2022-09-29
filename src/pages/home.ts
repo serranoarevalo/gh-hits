510962bee3d1d0917a873093484e052202c833a2
니꼬 <nico@nomadcoders.co>
2022-09-29 15:58:56 +0900

    home

---
+++ src/pages/home.ts
@@ -0,0 +1,15 @@
+export const home = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>gh hits</title>
+  </head>
+  <body>
+    <h1>gh hits</h1>
+    <h3>github profile view counter.</h3>
+  </body>
+</html>
+`;
