7011928b7727550608180a5d8864c5771063dd2a
니꼬 <nico@nomadcoders.co>
2022-09-29 20:20:37 +0900

    Pink Pink

--- src/utils.ts
+++ src/utils.ts
@@ -5,19 +5,19 @@ export const makeStatusResponse = (status: number) =>
 
 export const makeBadge = (hits: number) => {
   const formatted = new Intl.NumberFormat("kr-KO").format(hits);
-  const width = `Profile Views: ${formatted}`.length * 6.5;
+  const width = `Views  ${formatted}`.length * 6.5;
   const svg = `
   <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="${
     width + 10
-  }" height="20" role="img" aria-label="Profile Views: ${formatted}}">
-    <title>Profile Views: ${formatted}}</title>
+  }" height="20" role="img" aria-label="Views ${formatted}}">
+    <title>Views ${formatted}}</title>
     <g shape-rendering="crispEdges">
-      <rect x="0" width="${width + 50}" height="20" fill="black"/>
+    <rect x="0" width="${width + 50}" height="20" fill="#ff2e88"/>
     </g>
     <g fill="#fff" text-anchor="start" font-family="Verdana,Geneva,DejaVu Sans,sans-serif" text-rendering="geometricPrecision" font-size="110">
       <text x="50" y="140" transform="scale(.1)" fill="#fff" textLength="${
         width * 10
-      }">Profile Views: ${formatted}</text>
+      }">Views  ${formatted}</text>
     </g>
   </svg>
   `;
